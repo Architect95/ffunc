31fe6fe428d25a886d1e0ed343bf271834448cf4
Architect95 <71432761+Architect95@users.noreply.github.com>
2021-06-06 19:09:50 +0100

    Moved inline substr_utils fns into header file

--- src/substr_utils.h
+++ src/substr_utils.h
@@ -105,67 +105,226 @@ typedef struct {
 } WCharFromMB;
 
 
-static inline void 
-fillStringVector(
-  SEXP output, R_xlen_t loop_start, R_xlen_t loop_end, 
-  CharLenCE *const substrings);
+static inline void fillStringVector(SEXP output, R_xlen_t loop_start, 
+                                    R_xlen_t loop_end, 
+                                    CharLenCE *const substrings) {
+  // Most of the runtime of fsubstr() is spent in this function.
   
-static FORCE_INLINE void
-warnIfVectorArgRecycled(
-  const char * restrict arg_name, const R_xlen_t arg_len, 
-  const R_xlen_t len_to_match);
+  // Creating the CHARSXPs cannot be done in parallel because R's string
+  // hash table is not thread-safe. SET_STRING_ELT is not thread-safe
+  // either because it updates the ref counts of the strings in the hash table,
+  // and if the same string appears in two threads then a count could be
+  // modified separately in two threads at once.
+  
+  for (R_xlen_t i = loop_start; i < loop_end; ++i) {
+    
+    CharLenCE elt = substrings[i - loop_start];
+    
+    // There are checks in mkCharLenCE() that are not needed here, but there
+    // is no way to skip them whilst still using that function.
+    bool ptr_not_null = (elt.str_ptr != NULL);
+    SEXP str = (ptr_not_null ? mkCharLenCE(elt.str_ptr, elt.nbytes, elt.enc)
+                             : ((elt.nbytes == -1) ? NA_STRING : R_BlankString));
+    SET_STRING_ELT(output, i, str);
+  }
+}
+
+static FORCE_INLINE void 
+warnIfVectorArgRecycled(const char * restrict arg_name, const R_xlen_t arg_len,
+                        const R_xlen_t len_to_match) {
+                          
+  if ((arg_len < len_to_match) && ((len_to_match % arg_len) != 0)) {
+    warning("Length of %s is not a factor of length of vector; "
+            "%s will be partially recycled.", arg_name, arg_name);
+  }
+}
+
 
 static FORCE_INLINE StrIndex
-mbCount(
-  int count, const int i_stop, const char *str, 
-  const char *restrict const end, mbstate_t *const restrict mb_st_ptr);
+mbCount(int count, const int i_stop, 
+        const char *str, const char *restrict const end, 
+        mbstate_t *const restrict mb_st_ptr) {
   
-static FORCE_INLINE StrIndex 
-mbRecount(
-  int count, const int i_stop, const char *str, 
-  const char *restrict const end, mbstate_t *const restrict mb_st_ptr);
+  for (; count < i_stop && str < end; ++count) {
+    str += Mbrtowc(NULL, str, MB_CUR_MAX, mb_st_ptr);
+  }
+
+  return (const StrIndex) {count, str};
+}
+
+static FORCE_INLINE StrIndex
+mbRecount(int count, const int i_stop, 
+          const char *str, const char *restrict const end, 
+          mbstate_t *const restrict mb_st_ptr) {
   
+  // Uses mbrtowc() without error checking. For use on strings that have already
+  // been error-checked with Mbrtowc().
+  
+  for (; count < i_stop && str < end; ++count) {
+    str += mbrtowc(NULL, str, MB_CUR_MAX, mb_st_ptr);
+  }
+
+  return (const StrIndex) {count, str};
+}
+
 static FORCE_INLINE WCharFromMB
-stateShiftAndFirstChar(
-  const char *str, mbstate_t *const mb_st_ptr);
+stateShiftAndFirstChar(const char *str, mbstate_t *const mb_st_ptr) {
+  // Convert first character of substring to wchar_t and back to multibyte chars
+  // in order to store the appropriate state change control characters:
   
-static FORCE_INLINE long 
-writeShiftStateSubstrToBuffer(
-  const wchar_t first_substr_char, const char *restrict const rest_of_sub, 
-  const int rest_nbytes);
+  // We know str != end here, so no need to test this.
   
-static FORCE_INLINE CharLenCE 
+  wchar_t first_substr_char;
+  str += Mbrtowc(&first_substr_char, str, MB_CUR_MAX, mb_st_ptr);
+
+  return (const WCharFromMB) { first_substr_char, str };
+}
+
+static FORCE_INLINE long
+writeShiftStateSubstrToBuffer(const wchar_t first_substr_char, 
+                              const char *restrict const rest_of_sub, 
+                              const int rest_nbytes) {
+                                
+  int buffer_len = MB_CUR_MAX + rest_nbytes;
+  char *const buffer = allocStringBuffer(buffer_len, &strbuff);
+  
+  mbstate_t sub_mb_st;
+  memset(&sub_mb_st, '\0', sizeof(mbstate_t));
+  
+  // Write the substring to the buffer:
+  char *out = buffer;
+  out += wcrtomb(out, first_substr_char, &sub_mb_st);  // First char
+  const char new_first_mb_char_width = out - buffer;
+  memcpy(out, rest_of_sub, rest_nbytes);               // Rest of substring
+  
+  return new_first_mb_char_width + rest_nbytes;
+}
+
+#define SUBSTR_AFTER_SHIFT_STATE_IS_SET(count_fn)                             \
+                                                                              \
+  StrIndex end_of_sub = count_fn(cur_count, i_stop, rest_of_sub, end, &mb_st);\
+                                                                              \
+  const ptrdiff_t rest_nbytes = end_of_sub.ptr - rest_of_sub;                 \
+                                                                              \
+  long new_nbytes =                                                           \
+    writeShiftStateSubstrToBuffer(first_substr_char, rest_of_sub,             \
+                                  rest_nbytes);                               \
+                                                                              \
+  return (const CharLenCE) {strbuff.data, new_nbytes, encoding};              \
+
+
+static FORCE_INLINE CharLenCE
 substrAfterShiftStateIsSet(
   const char *rest_of_sub, const cetype_t encoding, mbstate_t mb_st, 
   const char *const end, const int cur_count, const int i_stop, 
-  const wchar_t first_substr_char);
+  const wchar_t first_substr_char) {
   
-static FORCE_INLINE CharLenCE 
+  SUBSTR_AFTER_SHIFT_STATE_IS_SET(mbCount)
+}
+
+static FORCE_INLINE CharLenCE
 substrAfterShiftStateIsSetUnguarded(
   const char *rest_of_sub, const cetype_t encoding, mbstate_t mb_st, 
   const char *const end, const int cur_count, const int i_stop,
-  const wchar_t first_substr_char);
+  const wchar_t first_substr_char) {
+  
+  SUBSTR_AFTER_SHIFT_STATE_IS_SET(mbRecount)
+}
 
-static FORCE_INLINE CharLenCE 
-substrWithinShiftState(
-  const char *start_of_sub, const cetype_t encoding, mbstate_t mb_st, 
-  const char *const end, int nchars, const int i_stop);
+static FORCE_INLINE CharLenCE
+substrWithinShiftState(const char *start_of_sub, const cetype_t encoding, 
+  mbstate_t mb_st, const char *const end, int nchars, const int i_stop) {
+  // Cannot directly copy the bytes of the substring in the original
+  // multibyte substring, because the state changing control characters
+  // for the current multibyte state occure before start_point in the
+  // original string.
+  
+  if (start_of_sub == end) { return EMPTY_STRING_CLC; }
+  
+  WCharFromMB first_substr_char = stateShiftAndFirstChar(start_of_sub, &mb_st);
+  ++nchars;
+
+  return substrAfterShiftStateIsSet(first_substr_char.ptr_after, encoding, 
+    mb_st, end, nchars, i_stop, first_substr_char.wchar);
+}
+
 
 
 
 // Functions that return substrings:
 
-static FORCE_INLINE CharLenCE
-substrUtf8(
-  const char *str, const int nbytes, const cetype_t encoding,
-  const int i_start, const int i_stop, const bool skip_validity_check);
+static FORCE_INLINE CharLenCE 
+substrUtf8(const char *str, const int nbytes, const cetype_t encoding,
+           const int i_start, const int i_stop,
+           const bool skip_validity_check) {
   
-static FORCE_INLINE CharLenCE
-substrSingleByteChars(
-  const char *str, const int nbytes, const cetype_t encoding,
-  const int i_start, const int i_stop);
+  if (!skip_validity_check && !isValidUtf8(str, nbytes)) {
+    error("Elt is invalid UTF8-encoded string \'%s\'", str);
+  }
   
-static FORCE_INLINE CharLenCE
-substrMultiByteChars(
-  const char *str, const int nbytes, const cetype_t encoding,
-  const int i_start, const int i_stop);
+  const int just_before_start = i_start - 1;
+  const char *const end = str + nbytes;
+  // Increment the str pointer past all the characters at the front to be
+  // omitted:
+  int j = 0;
+  for (; j < just_before_start && str < end; ++j) {
+    // The comparison to end is needed because there's no check whether a
+    // given char is the null terminator.
+    str += utf8clen(*str);
+  }
+  const char *const start_point = str;
+  for (; j < i_stop && str < end; ++j) {
+    str += utf8clen(*str);
+  }
+  return (const CharLenCE) {start_point, (int)(str - start_point), encoding};
+}
+
+static FORCE_INLINE CharLenCE 
+substrSingleByteChars(const char *str, const int nbytes,
+                      const cetype_t encoding,
+                      const int i_start, const int i_stop) {
+  
+  if (i_stop >= nbytes) {
+    return (const CharLenCE) {str + i_start -1, nbytes - i_start +1, encoding};
+  } else {
+    return (const CharLenCE) {str + i_start -1, i_stop - i_start +1, encoding};
+  }
+  // A branchless alternative using min(i_stop, nbytes) is possible, but it is
+  // empirically slower in benchmarking examples (possibly due to regular data
+  // leading to accurate branch prediction).
+}
+
+
+static FORCE_INLINE CharLenCE 
+substrMultiByteChars(const char *const start_of_str, const int nbytes,
+                     const cetype_t encoding,
+                     const int i_start, const int i_stop) {
+  
+  mbstate_t mb_st;
+  memset(&mb_st, 0, sizeof(mbstate_t));
+  
+  const char *restrict str = start_of_str;
+
+  const int just_before_start = i_start - 1;
+  const char *const end = str + nbytes;
+  
+  // Move str to the start of the substring:
+  StrIndex start_of_sub = mbCount(0, just_before_start, str, end, &mb_st);
+  str = start_of_sub.ptr;
+  
+  if (!mbsinit(&mb_st)) {  
+    return substrWithinShiftState(str, encoding, mb_st, end, start_of_sub.index,
+                                  i_stop);
+  }
+  
+  if (i_stop >= nbytes) {
+    return (const CharLenCE) {str, nbytes -(int)(str - start_of_str), encoding};
+  } else {
+    
+    StrIndex end_of_sub = mbCount(start_of_sub.index, i_stop, str, end, &mb_st);
+    
+    int new_nbytes = end_of_sub.ptr - start_of_sub.ptr;
+    
+    return (const CharLenCE) {start_of_sub.ptr, new_nbytes, encoding};
+  }
+}
